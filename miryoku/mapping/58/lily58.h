531b4ab7d770dbfa8174db20c9721244582a228a
James <jamesfleeting3@gmail.com>
2023-10-07 16:18:19 +0100

    Update lily58.h

--- miryoku/mapping/58/lily58.h
+++ miryoku/mapping/58/lily58.h
@@ -11,11 +11,11 @@
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-XXX  XXX  XXX  XXX  XXX  XXX                 XXX  XXX  XXX  XXX  XXX  XXX \
-XXX  K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
-               XXX  K32  K33  K34       K35  K36  K37  XXX
+XXX  XXX  K01  K02  K03  K04                 K05  K06  K07  K08  XXX  XXX \
+XXX  K00  K11  K12  K13  K14                 K15  K16  K17  K18  K09  XXX \
+XXX  K10  K21  K22  K23  K24                 K25  K26  K27  K28  K19  XXX \
+XXX  K20  XXX  XXX  K32  K33  XXX       XXX  K36  K37  XXX  XXX  K29  XXX \
+               XXX  XXX  XXX  K34       K35  XXX  XXX  XXX
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 52 53
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 54 55
